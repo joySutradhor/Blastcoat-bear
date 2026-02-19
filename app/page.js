e7b3a21102de9bbac99090c31c7fcd46358576eb
joySutradhor <joysutradhorcmt@gmail.com>
2026-02-19 15:05:55 +0600

    check

--- app/page.js
+++ app/page.js
@@ -1,17 +1,9 @@
 
-import Footer from "./_components/Footer";
-import GallerySection from "./_components/GallerySection";
-import HeroSection from "./_components/Herosection";
-import Navbar from "./_components/Navbar";
-import ServicesSection from "./_components/ServicesSection";
-import TestimonialMarquee from "./_components/TestimonialMarquee";
-import TrustSection from "./_components/TrustSection";
-import WhyChooseUs from "./_components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="bg-[#111]">
-      <h1>hello</h1>
+      <h1>hello sd</h1>
       {/* <Navbar /> */}
       {/* <HeroSection />
       <WhyChooseUs/>
